964bcdf109112ecd87e244e8d8da150be2986bee
saifulemon <463saiful0@gmail.com>
2022-02-11 16:42:11 +0600

    Array destructing

---
+++ array.js
@@ -0,0 +1,5 @@
+// Array Destructing 
+let fruits = ['Apple', 'Mango', 'Banana', 'Orange', 'Grape'];
+
+let [fruit1, fruit2, fruit3, fruit4, fruit5] = fruits;
+console.log(fruit1, fruit2, fruit3, fruit4, fruit5);
