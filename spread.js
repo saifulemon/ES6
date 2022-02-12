0f8c21dcac4f29fa9482ca42538aa5b0f52b1b18
saifulemon <463saiful0@gmail.com>
2022-02-12 16:26:17 +0600

    spread operator concept of ES6

--- spread.js
+++ spread.js
@@ -21,4 +21,16 @@ let person = {
 }
 
 let newPerson = {...person, dob: '26-09-2002'};
-console.log(newPerson);
+//console.log(newPerson);
+
+
+let numbers = [24, 65, -1, 96, 5, 87];
+//console.log(Math.max(...numbers));
+
+let person1 = ['Saiful', 'Emon'];
+
+let test1 = (fName, lName) => {
+    console.log(`Hello ${fName} ${lName}`);
+}
+// test1(person1[0], person1[1]);
+test1(...person1);  // using spread operator
