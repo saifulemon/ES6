85742438680915db3ca96ef72be6c43369df86aa
saifulemon <463saiful0@gmail.com>
2022-02-12 18:03:29 +0600

    ES6 constructor

---
+++ constructor.js
@@ -0,0 +1,24 @@
+// ES5 Constructor
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// Person.prototype.getName = function () {
+//     console.log(this.name);
+// }
+
+
+// ES6 Class
+class Person {
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getName() {
+        console.log(this.name);
+    }
+}
+
+let person1 = new Person('Safiul', 56);
+console.log(person1.getName());
