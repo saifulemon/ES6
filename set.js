12fa6098f419d32ae89165e18dc9cc091804f304
saifulemon <463saiful0@gmail.com>
2022-02-15 11:46:07 +0600

    set function

--- set.js
+++ set.js
@@ -3,31 +3,28 @@ let mySet = new Set([1, 3, 5, 7]);
 mySet.add('Hello');
 mySet.delete(1);
 mySet.add(4);
-
-
-// console.log(mySet.has(1));
+console.log(mySet.has(1));
 
 
 
 // Iterating Sets
 for (x of mySet) {
-    // console.log(x);
+    console.log(x);
 }
 
 let iter = mySet.entries();
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
 
 
 for (let [x] of mySet.entries()) {
-    // console.log(x);
+    console.log(x);
 }
 
 iter = [...mySet.entries()];
-// console.log(iter);
-
+console.log(iter);
 
 mySet.forEach(value => console.log(value));
